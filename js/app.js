8ebe12c73e0ff6c57cc19dfac1e8e8fb77614639
Manrique Iriarte <m_iriarte33@yahoo.com>
2018-06-17 23:07:23 -0400

    Added highscore functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let score = 0; 
+let timesWon = 0; 
 window.onload = function() {
 	alert('Welcome to my version of the Frogger arcade game! Press "OK" to play');
 }
@@ -34,7 +35,7 @@ class Enemy {
 	// Update the enemy's position, required method for game
     // Parameter: dt, a time delta between ticks
 	update(dt) {
-		// You should multiply any movement by the dt parameter which will ensure the game runs at the same speed for all computers
+		// multiply any movement by the dt parameter which will ensure the game runs at the same speed for all computers
 		this.x += this.speed * dt; 
 		if (this.x > 505) { //redo the enemies position once it goes off canvas
 			this.x = 0;
@@ -46,8 +47,29 @@ class Enemy {
 		//recieved help from udacity student-helpers 'lloan' and 'mvalentin' on slack
 		const center = this.width/2; 
 		if (this.x + center > player.x && this.x - center < player.x && this.y + center > player.y && this.y - center < player.y) { // compare if the player is in the this's "personal space" i.e hitting its center
+			let score1 = document.querySelector('#high-score-1');
+			let score2 = document.querySelector('#high-score-2');
+			let score3 = document.querySelector('#high-score-3'); 
+			if (score > highScores[0]) {
+				highScores[2] = highScores[1]; 
+				highScores[1] = highScores[0]; 
+				highScores[0] = score;
+				score3.textContent = score2.textContent; 
+				score2.textContent = score1.textContent; 
+				score1.textContent = `${score}`;
+			} else if (score > highScores[1]) {
+				highScores[2] = highScores[1]; 
+				highScores[1] = score;
+				score3.textContent = score2.textContent; 
+				score2.textContent = `${score}`;
+			} else if (score > highScores[2]) {
+				highScores[2] = score; 
+				score3.textContent = `${score}`;
+			}
+			console.log(highScores); 
 			player.reset();
 			score = 0; 
+			timesWon = 0; 
 			allEnemies.splice(3); // restart enemies to 3 if the player loses
 			gems.pop() // remove gem if the player loses
 			scoreCounter.textContent = `Score: ${score}`; 
@@ -60,8 +82,7 @@ class Enemy {
 	}
 }
 
-// Now write your own player class
-// This class requires an update(), render() and a handleInput() method.
+//class for user-controlled player
 class Player {
 	constructor() {
 		this.sprite = 'images/char-boy.png';
@@ -70,17 +91,16 @@ class Player {
     	this.y = 390;
 		this.width = 101; 
 		this.height = 171; 
-		this.timesWon = 0; 
 	}
 	
 	update() {
 		// reset the player if they win and update their score 
 		if (this.y == -35) {
 			score++; 
-			this.timesWon++; 
+			timesWon++; 
 			scoreCounter.textContent = `Score: ${score}`;
 			this.reset();
-			if (this.timesWon % 5 === 0 && this.timesWon !== 0 && this.timesWon !== 45) { //We need to cap out the amount of enemies so that the game never gets too difficult 
+			if (timesWon % 5 === 0 && timesWon !== 0 && timesWon !== 45) { //We need to cap out the amount of enemies so that the game never gets too difficult 
 				createEnemies(1);
 			}
 			if (score % 3 === 0 && score !== 0) {
@@ -155,16 +175,14 @@ class Gem {
 	}
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+//instantiating objects
 let player = new Player(); 
 let allEnemies = [];
 let gems = []
+let highScores = [0, 0, 0]; 
 let scoreCounter = document.querySelector('#score');
 createEnemies();
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+//listens to keypresses to send to Player.handleInput
 document.addEventListener('keydown', function(e) {
 	//prevent pages from scrolling 
 	if (e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40) {
